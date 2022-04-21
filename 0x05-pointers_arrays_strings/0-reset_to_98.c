199fb6f10e1ba26151ad881127a63392fca3fe40
solexod <solexod12@gmail.com>
2022-04-20 17:03:32 -0700

    Ny new commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,12 @@
-#include "main.h"
+#include "main.h"
 
-/** 
- * reset_to_98 - function that takes a pointer to an integer parameter 
- * @n: integer pointer 
- * Return: 0 success
- */ 
-   
-void reset_to_98(int *n) 
+/**
+ * reset_to_98 - function that takes a pointer to an integer parameter
+ * @n: integer pointer
+ * Return: 0 Success
+ */
+void reset_to_98(int *n)
+
 {
-		  *n = 98; 		    
+		*n = 98;
 }
