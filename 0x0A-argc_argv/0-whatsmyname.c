3c5a88f08dfae1ddf1432c774afcb5da9d94ccd3
Solexod <solexod12@gmail.com>
2022-05-04 07:45:08 -0700

    my commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
+
+
 /**
  * main = prints its name,follow by a new line.
  * @argc:number of command line arguements.
  * @argv: array that contains the program command line arguements.
- * reture: 0 _ success.
+ * reture: 0 - success.
  */
+
 int main(int argc _attribute_((unused)), char *argv[])
 {
 	print("%s\n", argv[0]);
-	return n(0);
+	return (0);
 }
