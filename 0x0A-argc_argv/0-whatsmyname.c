00615c033d8b5f6c6c4531a29f6ef69dd2d8076e
Solexod <solexod12@gmail.com>
2022-05-04 10:02:16 -0700

    My commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 
-
 /**
- * main = prints its name,follow by a new line.
- * @argc:number of command line arguements.
- * @argv: array that contains the program command line arguements.
- * reture: 0 - success.
+ * main - prints its name, followed by a new line.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
  */
 
-int main(int argc _attribute_((unused)), char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	print("%s\n", argv[0]);
-	return (0);
+printf("%s\n", argv[0]);
+return (0);
 }
--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,21 +9,18 @@
  * @argv: array that contains the program command line arguments.
  * Return: 0 - success.
  */
+
 int main(int argc, char *argv[])
 {
 int i, j, add = 0;
-
 for (i = 1; i < argc; i++)
 {
-for (j = 0; argv[i][j] != '\0'; j++)
+								for (j = 0; argv[i][j] != '\0'; j++)
+{															if (!isdigit(argv[i][j]))
 {
-															if (!isdigit(argv[i][j]))
-{
-																	printf("Error\n");
-																        return (1);
-																	}													
-{
-add += atoi(argv[i]);
+																				printf("Error\n");																												return (1);
+}
+}										add += atoi(argv[i]);
 }
 printf("%d\n", add);
 return (0);
